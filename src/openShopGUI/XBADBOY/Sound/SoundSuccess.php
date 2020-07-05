79fa377b114ec683576ac8ffc7b0979552f76c91
Alfiangaming <57852047+XBadBoy@users.noreply.github.com>
2020-07-05 15:24:34 +0800

    Add files via upload

---
+++ src/openShopGUI/XBADBOY/Sound/SoundSuccess.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace openShopGUI\XBADBOY\Sound;
+
+use pocketmine\level\sound\Sound;
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+class SoundSuccess extends Sound{
+
+    public function encode() : PlaySoundPacket{
+        $pk = new PlaySoundPacket();
+        $pk->soundName = "note.bell";
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->volume = 400;
+        $pk->pitch = 1;
+        return $pk;
+    }
+}
